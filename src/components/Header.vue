5878ff3e63eb15f6a6765ab4200680edfc28704f
jai <corpuz.jianna@gmail.com>
2026-06-12 15:23:45 +0800

    new project created

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import {ref} from 'vue'
+    const title = ref('Quote Generator')
+
+</script>
+
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+
+<style scoped>
+    header {
+        display: flex;  
+        font-family:'Georgia', serif;
+        color:#CCD6D9;
+        margin-bottom:25px;
+        justify-content: center;
+        align-items: center;
+        font-size: 30px;
+    }
+</style>
